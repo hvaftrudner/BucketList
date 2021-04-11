ee2b959f81fad38f3b6fb396e73c1f9872a658aa
hvaftrudner <krill_eriksson@hotmail.com>
2021-04-11 22:31:18 +0200

    Initial Commit

---
+++ BucketList Examples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList Examples
+//
+//  Created by Kristoffer Eriksson on 2021-04-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
